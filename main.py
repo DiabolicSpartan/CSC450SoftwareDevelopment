6ba057470cdd019e4b6537d44251fd8a93a90bf6
Justin Snider <46825099+DiabolicSpartan@users.noreply.github.com>
2021-01-19 14:39:23 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,64 +1,168 @@
+#import packages
 import cv2
-import numpy as np
+import time
+from datetime import datetime
+import imutils
+from imutils.video import VideoStream
+import sys
+import argparse
+import ctypes
 
-# Will accept video feed from first (zero-indexed) camera available to system (Usually a webcam)
-cap = cv2.VideoCapture(0)
-# Background subtraction algorithm MOG
-bgReduct = cv2.createBackgroundSubtractorMOG2()
-# Save the resolution of the camera and print it to the console
-frameWidth = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
-frameHeight = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
-print(frameWidth, frameHeight)
-# Continue loop as long as the video feed meets the resolution requirements
-while (True):
-    # Returns true if the video feed still has frames left and then returns each frame from the video feed
-    ret, frame = cap.read()
-    # Convert each frame to grayscale
-    grayscale = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    # Create the foreground mask and apply it to the converted frame
-    fgmask = bgReduct.apply(grayscale)
-    # Display converted and masked feed
-    cv2.imshow('vidFeed', fgmask)
-    # Wait for a 'Q' keypress, then break the loop
-    # A bitwise AND operation is used to return only the last byte (0xFF) from the key pressed
-    # The last byte will always be same for each key, but modifier keys can change bits further down
-    # Therefore by accepting only the last byte, any changes made by modifier keys will be ignored
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+#Globals
+startAlertTimer = 0
+defaultTimer = 10
+currentAlertTimer = defaultTimer
+alertTimerThreshold = defaultTimer
+statusText = "No movement detected."
+previousStatusText = "NULL"
+prevprevStatusText = "Null"
+alertSent = False
+fallDetected = False
+personInFrame = False
 
+#This function inizilizes the program
+def initializeProgram():
+    try:
+        createLogFile()
+        ap = argparse.ArgumentParser()
+        ap.add_argument("-a", "--min-area", type=int, default=4000, help="minimum area size")
+        args = vars(ap.parse_args())
+        firstFrame= None
+        capture = startCapture()
+        writeToLog("Program fully initilized!")
+        frameWidth = int(capture.get(cv2.CAP_PROP_FRAME_WIDTH))
+        frameHeight = int(capture.get(cv2.CAP_PROP_FRAME_HEIGHT))
+        return args, firstFrame, capture, frameWidth, frameHeight
+    except:
+        writeToLog("ERROR: Initilization failed!")
+        sys.exit()
 
-#Loops
-#detectMovement is a loop that contains compareFrames and checks to see if movement has occured if True, executes detectHumanoid
-#def detectMovement():
-#detectHumanoid is a lopp that contains compareFrames and checks if the source of movement is humoid, rather than a household pet. If True, executes followHumanoid
-#def detectHumanoid()
-#followHumanoid is a loop that follows the humanoid subject until it leaves view or fall is detected. If humanoid leaves, program executes resetProgram. If humanoid falls, program executes sendAlert
-#def followHumanoid()
+#This function trys to create a log file
+def createLogFile():
+    try:
+        logFile = open("Test_Event_Log.txt", "a+")
+        logFile.write("********************New Initialization********************\n\n")
+        logFile.close()
+        writeToLog("Event Log Initialized!")
+    except:
+        print("Log File failed to generate. Terminating program...")
+        sys.exit()
 
-#Functions
-#compareFrames takes the previous frame and current frame and compares them for the above loops
-#def compareFrames()
-    #We will want to pass the previous frame and current frame into this function. Recursion?
-#sendAlert executes if subjectFallen = True. Afterwards, programs executes resetProgram
-#def sendAlert()
-    #Are we wanting to just make a pop-up message that says "fall detected?"
-    #resetProgram()
-#Resets all states to default to start in outer "main" loop
-#def resetProgram()
-    #movementDetected = False
-    #subjectInFrame = False
-    #subjectFallen = False
+#This function is called when an event is needed to be written to a log
+def writeToLog(strEvent):
+    logFile = open("Test_Event_Log.txt", "a+")
+    logFile.write("["+getTimeStamp()+"] - "+strEvent+"\n")
+    logFile.close()
 
-#States (Boolean)
-#movementDetected = False
-#subjectInFrame = False
-#subjectFallen = False
+#This function acquires a timestamp
+def getTimeStamp():
+    now = datetime.now()
+    now = now.strftime("%d%b%Y %H%M%S")
+    return now
 
+#This function trys to initialized the camera feed
+def startCapture():
+    try:
+        capture = cv2.VideoCapture(0)
+        writeToLog("Camera feed sucessfully acquired!")
+        return capture
+    except:
+        writeToLog("No camera detected. Closing Program...")
+        sys.exit()
 
+#This function implements processes the video
+def processVideo(frame, args, firstFrame):
+    global statusText, previousStatusText, prevprevStatusText
+    prevprevStatusText = previousStatusText
+    previousStatusText = statusText
+    statusText = "No movement detected."
+    frame = imutils.resize(frame, width=500)
+    frameGray = cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY)
+    frameBlur = cv2.GaussianBlur(frameGray, (21, 21), 5)
+    if firstFrame is None:
+        firstFrame = frameBlur
+    frameDelta = cv2.absdiff(firstFrame, frameBlur)
+    frameThresh = cv2.threshold(frameDelta, 20, 255, cv2.THRESH_BINARY)[1]
+    frameDilate = cv2.dilate(frameThresh, None, iterations=15)
+    contoursFind = cv2.findContours(frameDilate.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    contoursGrab = imutils.grab_contours(contoursFind)
+    for c in contoursGrab:
+        if cv2.contourArea(c) < args["min_area"]:
+            continue
+        (x, y, w, h) = cv2.boundingRect(c)
+        cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+        monitorSubject(w,h)
+    enterExit()
+    return frame, firstFrame
 
+#This function monitors a subject when in frame
+def monitorSubject(xaxis, yaxis):
+    x = int(xaxis)
+    y = int(yaxis)
+    global statusText, alertSent, alertTimerThreshold, currentAlertTimer, startAlertTimer
+    if alertSent == True:
+        statusText = "Alert has been Sent. Awaiting help..."
+    elif x>y:
+        if currentAlertTimer <= 0:
+            writeToLog("Alert has been Sent. Awaiting help...")
+            startAlertTimer = 0
+            currentAlertTimer = alertTimerThreshold
+            alertSent = True
+        elif startAlertTimer != 0:
+            statusText = "Subject appears to have fallen."
+            currentAlertTimer = alertTimerThreshold - (time.time() - startAlertTimer)
+        else:
+            statusText = "Subject appears to have fallen."
+            startAlertTimer = time.time()
+            writeToLog("Suspected fall has occured. An alert will be sent if subject does not stand back up within 10 seconds.")
+    elif x<y:
+        if currentAlertTimer < alertTimerThreshold:
+            startAlertTimer = 0
+            currentAlertTimer = alertTimerThreshold
+            writeToLog("Subject has returned to an upright position.")
+            statusText = "Subject is safe."
+        elif currentAlertTimer == alertTimerThreshold:
+            statusText = "Subject is safe."
 
+#Entry/Exit logging. Designed misread frames don't incorrectly log
+def enterExit():
+    global prevprevStatusText, statusText
+    if prevprevStatusText == "No movement detected." and previousStatusText == "No movement detected." and statusText == "Subject is safe.":
+        writeToLog("Subject has entered the camera's view.")
+    elif prevprevStatusText == "Subject is safe." and previousStatusText == "No movement detected." and statusText == "No movement detected.":
+        writeToLog("Subject has left the camera's view.")
 
+#This function sends an alert
+def sendAlert():
+    statusText = "Awaiting Response..."
 
-# Important to release the capture after each time the program is finished running
-cap.release()
-cv2.destroyAllWindows()
+#This function displays the Video
+def displayVideo(frame):
+    #Add timestamp to the video then display
+    frame = cv2.putText(frame, "Date/Time: {}".format(getTimeStamp()), (5, 275), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 1)
+    cv2.putText(frame, "Room Status: {}".format(statusText), (5, 15), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 1)
+    cv2.imshow("Fall Alert Detection System", frame)
+
+#This function closes the program
+def closeProgram(capture):
+    writeToLog("Terminating Program...\n\n\n")
+    capture.release()
+    cv2.destroyAllWindows()
+    sys.exit()
+
+#This is the main function of the program
+def main():
+    args, firstFrame, capture, frameWidth, frameHeight = initializeProgram()
+    while (True):
+        ret, frame = capture.read()
+        if frame is None:
+            break
+        frame, firstFrame = processVideo(frame, args, firstFrame)
+        displayVideo(frame)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+    closeProgram(capture)
+
+#Script
+if __name__ == "__main__":
+    main()
